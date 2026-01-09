a185b6d5e8d25386749f56b6d091d797270af457
nishant561 <nishantbaruwal560@gmail.com>
2026-01-09 11:06:16 +0545

    Initial commit: setting up spring projct

---
+++ src/test/java/com/nishant/newsportal/NewsportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishant.newsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
